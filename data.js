bd7c42a0b23a17203d6ad9c4735b3711cc69dcb2
walimajid644 <147191282+walimajid644@users.noreply.github.com>
2025-09-02 03:09:17 +0500

    updated lists

--- data.js
+++ data.js
@@ -14,48 +14,48 @@ Description:`
 },
     {
 id: 2,
-Name: "Express",
+Name: "Prime Video",
 Type: "streamingService",
 imageUrl:"images/primelogo.jpeg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
-        <li>27 TO 30 DAYS DURATION TIME</li>
+        <li>Platinum Screen</li>
+        <li>Private Screen</li>
+        <li>Pin protected</li>
+        <li>Custom name</li>
       </ul> `,
 },
     {
 id: 3,
-Name: "Nord",
-Type: "Vpn",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "Disney+",
+Type: "streamingService",
+imageUrl:"images/Disney+logo.jfif",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
         <li>27 TO 30 DAYS DURATION TIME</li>
       </ul> `,
 },
     {
 id: 4,
-Name: "Netflix",
-Type: "Vpn",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "Apple TV+",
+Type: "streamingService",
+imageUrl:"images/AppleTVlogo.jfif",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
         <li>27 TO 30 DAYS DURATION TIME</li>
       </ul> `,
 },
     {
 id: 5,
-Name: "Netflix",
-Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "Canva",
+Type: "othertool",
+imageUrl:"images/Canvalogo.jfif",
 Description:`
       <ul>
         <li>SINGLE DEVICE SCREEN </li>
@@ -66,74 +66,74 @@ Description:`
 },
     {
 id: 6,
-Name: "Netflix",
-Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "Opplex/Bravo",
+Type: "iptv",
+imageUrl:"images/opplex.jpeg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
-        <li>27 TO 30 DAYS DURATION TIME</li>
+        <li>⁠Live Channels - 12500+</li>
+        <li>⁠Movies - 58000+</li>
+        <li>⁠Series - 3000+</li>
+        <li>⁠Required Speed: 10mbps</li>
       </ul> `,
 },
     {
 id: 7,
-Name: "Netflix",
+Name: "Geo WORLD Package",
 Type: "iptv",
-imageUrl:"images/Netflixlogo.jpeg",
+imageUrl:"images/geo.jpeg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
-        <li>27 TO 30 DAYS DURATION TIME</li>
+        <li>⁠Live Channels - 9000+</li>
+        <li>Movies - 67000+</li>
+        <li>⁠Series - 7000+</li>
+        <li>Required Speed: 10mbps</li>
       </ul> `,
 },
     {
 id: 8,
-Name: "Netflix",
+Name: "Geo Europe Package",
 Type: "iptv",
-imageUrl:"images/Netflixlogo.jpeg",
+imageUrl:"images/geo.jpeg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
-        <li>27 TO 30 DAYS DURATION TIME</li>
+        <li> ⁠Live Channels - 6000+</li>
+        <li>Movies - 60000+</li>
+        <li>Series - 4000+</li>
+        <li>Required Speed: 10mbps⁠</li>
       </ul> `,
 },
     {
 id: 9,
-Name: "Netflix",
-Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "Geo Asian Mini Package",
+Type: "iptv",
+imageUrl:"images/geo.jpeg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
-        <li>27 TO 30 DAYS DURATION TIME</li>
+        <li>⁠Live Channels - 4000+</li>
+        <li>Movies -30000+</li>
+        <li> ⁠Series - 2000+</li>
+        <li>Required Speed: 10mbps ⁠</li>
       </ul> `,
 },
     {
 id: 10,
-Name: "Netflix",
-Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "StarShare",
+Type: "iptv",
+imageUrl:"images/starshare.jpeg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
-        <li>27 TO 30 DAYS DURATION TIME</li>
+        <li>Live Channels - Rs. 9000+</li>
+        <li>⁠Movies - 33000+</li>
+        <li>Series - 5000+</li>
+        <li>Required Speed: 20mbps ⁠ </li>
       </ul> `,
 },
     {
 id: 11,
-Name: "Netflix",
-Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "B1G",
+Type: "iptv",
+imageUrl:"images/b1g.png",
 Description:`
       <ul>
         <li>SINGLE DEVICE SCREEN </li>
@@ -144,118 +144,417 @@ Description:`
 },
     {
 id: 12,
-Name: "Netflix",
-Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "MegaOTT",
+Type: "iptv",
+imageUrl:"images/mega.png",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
-        <li>27 TO 30 DAYS DURATION TIME</li>
+        <li>⁠Live Channels: 23200+</li>
+        <li> ⁠Movies: 103000+</li>
+        <li> ⁠Series: 29500+</li>
+        <li>Required Speed: 25mbps ⁠</li>
       </ul> `,
 },
     {
 id: 13,
-Name: "Netflix",
-Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "5G Live / Zain TV",
+Type: "iptv",
+imageUrl:"images/5glive.png",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
-        <li>27 TO 30 DAYS DURATION TIME</li>
+        <li>⁠Live Channels - 6000+</li>
+        <li>Movies - 38000+</li>
+        <li>⁠Series - 3000+</li>
+        <li>Required Speed: 20mbps</li>
       </ul> `,
 },
     {
 id: 14,
-Name: "Netflix",
-Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "Trex",
+Type: "iptv",
+imageUrl:"images/trexiptv.png",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
-        <li>27 TO 30 DAYS DURATION TIME</li>
+        <li>⁠Live Channels: 32300+</li>
+        <li>⁠Movies & Shows: 153000+</li>
+        <li>⁠Series: 29500+</li>
+        <li>Required Speed: 25mbps⁠</li>
       </ul> `,
 },
     {
 id: 15,
-Name: "Netflix",
+Name: "JioHotstar",
 Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+imageUrl:"images/jiohotstar.jpg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
         <li>27 TO 30 DAYS DURATION TIME</li>
       </ul> `,
 },
     {
 id: 16,
-Name: "Netflix",
-Type: "aitool",
-imageUrl:"images/Netflixlogo.jpeg",
+Name: "Peacock",
+Type: "streamingService",
+imageUrl:"images/peacock.jpg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
         <li>27 TO 30 DAYS DURATION TIME</li>
       </ul> `,
 },
     {
 id: 17,
-Name: "Netflix",
+Name: "Shudder",
 Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+imageUrl:"images/shudder.jpg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
         <li>27 TO 30 DAYS DURATION TIME</li>
       </ul> `,
 },
     {
 id: 18,
-Name: "Netflix",
+Name: "Zee5",
 Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+imageUrl:"images/zee5.jpg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
         <li>27 TO 30 DAYS DURATION TIME</li>
       </ul> `,
 },
     {
 id: 19,
-Name: "Netflix",
+Name: "Chaupal",
 Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+imageUrl:"images/chaupal.jpeg",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
         <li>27 TO 30 DAYS DURATION TIME</li>
       </ul> `,
 },
     {
 id: 20,
-Name: "Netflix",
+Name: "Curiosity Stream",
 Type: "streamingService",
-imageUrl:"images/Netflixlogo.jpeg",
+imageUrl:"images/curiositystream.png",
 Description:`
       <ul>
-        <li>SINGLE DEVICE SCREEN </li>
-        <li>RENEWAL SCREENPIN PROTECTED</li>
-        <li>LOGIN VIA SIGN IN CODE</li>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+    {
+id: 21,
+Name: "HBO Max",
+Type: "streamingService",
+imageUrl:"images/hbomax.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+    {
+id: 22,
+Name: "HULU",
+Type: "streamingService",
+imageUrl:"images/hululogo.jpeg",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+    {
+id: 23,
+Name: "Paramount",
+Type: "streamingService",
+imageUrl:"images/paramount.jpeg",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+    {
+id: 24,
+Name: "Shemaroome",
+Type: "streamingService",
+imageUrl:"images/shemaroome.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+    {
+id: 25,
+Name: "Shudder",
+Type: "streamingService",
+imageUrl:"images/shudder.jpg",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+    {
+id: 26,
+Name: "Sony",
+Type: "streamingService",
+imageUrl:"images/sony.jpeg",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+    {
+id: 27,
+Name: "Youtube Premium",
+Type: "streamingService",
+imageUrl:"images/youtube.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+    {
+id: 28,
+Name: "Capcut Pro",
+Type: "othertool",
+imageUrl:"images/capcutlogo.jfif",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 29,
+Name: "ChatGpt",
+Type: "aitool",
+imageUrl:"images/Chatgptlogo.jfif",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 30,
+Name: "Grammarly",
+Type: "othertool",
+imageUrl:"images/Grammarly Logo.jfif",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 31,
+Name: "Picsart",
+Type: "othertool",
+imageUrl:"images/picsartlogo.jpeg",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 32,
+Name: "QuillBot",
+Type: "aitool",
+imageUrl:"images/QuillBotlogo3.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 33,
+Name: "Udemy",
+Type: "othertool",
+imageUrl:"images/Udemy Logo.jpeg",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 34,
+Name: "Express",
+Type: "Vpn",
+imageUrl:"images/express.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 35,
+Name: "HMA",
+Type: "Vpn",
+imageUrl:"images/hma.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 36,
+Name: "HotSpot",
+Type: "Vpn",
+imageUrl:"images/hotspot.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 37,
+Name: "IP Vanish",
+Type: "Vpn",
+imageUrl:"images/ipvanish.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 38,
+Name: "Nord",
+Type: "Vpn",
+imageUrl:"images/nord.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 39,
+Name: "PIA",
+Type: "Vpn",
+imageUrl:"images/pia.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 40,
+Name: "Proton",
+Type: "Vpn",
+imageUrl:"images/proton.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 41,
+Name: "SurfShark",
+Type: "Vpn",
+imageUrl:"images/surfshark.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 42,
+Name: "Tunnel Bear",
+Type: "Vpn",
+imageUrl:"images/tunnelbear.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
+        <li>27 TO 30 DAYS DURATION TIME</li>
+      </ul> `,
+},
+{
+id: 43,
+Name: "Office 365",
+Type: "othertool",
+imageUrl:"images/office.png",
+Description:`
+      <ul>
+        <li>BEST RATES</li>
+        <li>PRIVATE SCREEN</li>
+        <li>4K/HD QUALITY</li>
         <li>27 TO 30 DAYS DURATION TIME</li>
       </ul> `,
 },
